c8208d5f960929a3f74341e5cd2478d1014cee32
INSEOP KIM <inseop0813@gmail.com>
2023-02-10 07:01:34 +0900

    6.GlobalStyles settings & install styled-reset
    - npm i styled-reset

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,9 +3,7 @@ import { useState } from "react";
 import styled, { css } from "styled-components";
 import { darkModeVar } from "../apollo";
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
+const Container = styled.div``;
 
 const Title = styled.h1`
   color: ${(props) => props.theme.fontColor};
