039ce525b8c714751e8566e6df198f670fe136b7
INSEOP KIM <inseop0813@gmail.com>
2023-02-09 22:42:38 +0900

    3.route settings & make screens

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
+import Login from "./screens/Login";
 
 function App() {
+  const isLoggedIn = false;
   return (
-  <div></div>
+    <Router>
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Login />} />
+          </>
+        )}
+      </Routes>
+    </Router>
   );
 }
 
