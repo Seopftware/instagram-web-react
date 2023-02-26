4f5cc307ecc713a0a67da43d9f0a5b796154ebbe
INSEOP KIM <inseop0813@gmail.com>
2023-02-26 21:04:26 +0900

    getAllFeeds

---
+++ src/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const instance = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1/",
+  headers: {
+    "X-CSRFToken": Cookies.get("csrftoken"),
+  },
+});
+
+export const getAllFeeds = () => {
+  return instance.get("feeds/").then((res) => res.data);
+};
